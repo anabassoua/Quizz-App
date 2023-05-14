045b141a1eb273bfd5b60a3d20479ce41d227d72
Askia <anabassoua@gmail.com>
2023-05-14 01:40:30 -0400

    Askia Nabassoua

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
+import Quizz from './Quizz'
 
 const App = () => {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>
-      heyyyyyyyyy
-    </h1>
+    <Quizz />
   )
 }
 
---
+++ client/src/Quizz.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+const Quizz = () => {
+  return (
+    <h1>Quizz</h1>
+  )
+}
+
+export default Quizz
