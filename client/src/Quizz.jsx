1df88d674fb0382cd25e6ae798769411c09791e4
Askia <anabassoua@gmail.com>
2023-05-15 23:33:21 -0400

    added html-entities package to transform into characters

--- client/src/Quizz.jsx
+++ client/src/Quizz.jsx
@@ -1,19 +1,26 @@
 import styled from "styled-components";
 import { useState, useEffect } from "react";
+// the npm package html-entities gives us the ability to transform entities into characters.
+import { decode } from "html-entities";
 
 const Quizz = () => {
+  const [quizzes, setQuizzes] = useState([]);
+
   useEffect(() => {
     fetch("https://opentdb.com/api.php?amount=10")
       .then((res) => res.json())
       .then((resData) => {
-        console.log(resData);
+        console.log(resData.results);
+        setQuizzes(resData.results);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
-  return <h1>Quizz</h1>;
+  return quizzes.map((quizz, index) => {
+    return <div key={index}>{decode(quizz.question)}</div>;
+  });
 };
 
 export default Quizz;
