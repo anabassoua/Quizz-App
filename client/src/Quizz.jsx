07e46e64b937da9afa6a1a45ed79e0869036f75d
Askia <anabassoua@gmail.com>
2023-05-17 01:44:31 -0400

    Askia Nabassoua

--- client/src/Quizz.jsx
+++ client/src/Quizz.jsx
@@ -23,6 +23,14 @@ const Quizz = () => {
     return <div>Loading...</div>; // Add a loading state while fetching data
   }
 
+  const handleClick = (answer) => {
+    if (answer === quizzes[currentQ].correct_answer) {
+      console.log("good answer");
+    } else {
+      console.log("wrong answer");
+    }
+    // console.log("clicked");
+  };
   const allAnswers = [
     ...quizzes[currentQ].incorrect_answers,
     quizzes[currentQ].correct_answer,
@@ -34,19 +42,50 @@ const Quizz = () => {
   console.log("random", randomAnswers);
 
   return (
-    <div>
-      <h1>Quizzle Of The Day 🤖</h1>
-      {/*Box of question below*/}
-      <h2>{quizzes.length > 0 && decode(quizzes[currentQ].question)}</h2>
+    <Div>
+      <Header>Quizzle Of The Day 🤖</Header>
+      <Container>
+        <h2>{quizzes.length > 0 && decode(quizzes[currentQ].question)}</h2>
 
-      {randomAnswers.map((quizz, index) => {
-        return <Box key={index}>{decode(quizz)}</Box>;
-      })}
-    </div>
+        {randomAnswers.map((answer, index) => {
+          return (
+            <Buttons key={index} onClick={() => handleClick(answer)}>
+              {decode(answer)}
+            </Buttons>
+          );
+        })}
+      </Container>
+    </Div>
   );
 };
-
-const Box = styled.div`
+const Div = styled.div`
   display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const Header = styled.h1`
+  text-align: center;
+`;
+
+const Buttons = styled.button`
+  margin-bottom: 15px;
+  border: 1px solid gray;
+  width: 400px;
+  height: 100px;
+  background: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
+
+const Container = styled.div`
+  border: 1px solid green;
+  padding: 15px;
+  width: 700px;
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 export default Quizz;
