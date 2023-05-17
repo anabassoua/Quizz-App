4c571b6f94edad1bdfb75f4ba68074a3edd1403f
Askia <anabassoua@gmail.com>
2023-05-17 00:58:17 -0400

    Askia Nabassoua

--- client/src/Quizz.jsx
+++ client/src/Quizz.jsx
@@ -5,6 +5,7 @@ import { decode } from "html-entities";
 
 const Quizz = () => {
   const [quizzes, setQuizzes] = useState([]);
+  const [currentQ, setCurrentQ] = useState(0);
 
   useEffect(() => {
     fetch("https://opentdb.com/api.php?amount=10")
@@ -18,12 +19,28 @@ const Quizz = () => {
       });
   }, []);
 
+  if (quizzes.length === 0) {
+    return <div>Loading...</div>; // Add a loading state while fetching data
+  }
+
+  const allAnswers = [
+    ...quizzes[currentQ].incorrect_answers,
+    quizzes[currentQ].correct_answer,
+  ];
+  console.log("test", allAnswers);
+
+  const randomAnswers = allAnswers.sort(() => Math.random() - 0.5);
+
+  console.log("random", randomAnswers);
+
   return (
     <div>
       <h1>Quizzle Of The Day 🤖</h1>
       {/*Box of question below*/}
-      {quizzes.map((quizz, index) => {
-        return <Box key={index}>{decode(quizz.question)}</Box>;
+      <h2>{quizzes.length > 0 && decode(quizzes[currentQ].question)}</h2>
+
+      {randomAnswers.map((quizz, index) => {
+        return <Box key={index}>{decode(quizz)}</Box>;
       })}
     </div>
   );
